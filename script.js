37b2b6102165fc92dec8f9f9d31fb9f65a5342bf
ivannhabilis <ivannsampaio@gmail.com>
2025-06-12 17:01:13 -0300

    Iniciando o projeto do curso

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Meu JavaScript está funcionando!");
+
+// Selecionando o título (você já fez isso)
+let titulo = document.getElementById('titulo-principal');
+
+// MODIFICANDO o texto dele
+titulo.textContent = "Título Alterado pelo JS!";
+
+// Selecionando o parágrafo (você já fez isso)
+let paragrafo = document.querySelector('.paragrafo');
+
+// MODIFICANDO a cor do texto dele
+paragrafo.style.color = 'red';
+paragrafo.style.fontSize = '20px'; // Aumentando a fonte
